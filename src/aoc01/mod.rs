9d2efde5f079db5824d6deaf94f45c2f3d3efede
fisehara <harald@balena.io>
2021-12-01 23:20:57 +0100

    second part of aoc01
    Signed-off-by: fisehara <harald@balena.io>

--- src/aoc01/mod.rs
+++ src/aoc01/mod.rs
@@ -2,10 +2,9 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-pub fn sonar(filename: String) -> u32{
+pub fn sonar(filename: String) -> u32 {
     // File hosts must exist in current path before this produces output
     let mut counter: u32 = 0;
-    
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
         let mut lastnum: u32 = u32::MAX;
@@ -19,6 +18,35 @@ pub fn sonar(filename: String) -> u32{
             }
         }
     }
+    return counter;
+}
+
+pub fn sonar_window(filename: String) -> u32 {
+    // File hosts must exist in current path before this produces output
+    let mut counter: u32 = 0;
+    let mut measures = Vec::new();
+
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+
+        for line in lines {
+            if let Ok(ip) = line {
+                let num: u32 = ip.parse().unwrap();
+                measures.push(num);
+            }
+        }
+    }
+
+    let mut lastnum: u32 = u32::MAX;
+
+    for idx in 0..measures.len() - 2 {
+        let num: u32 = &measures[idx] + &measures[idx + 1] + &measures[idx + 2];
+        if lastnum < num {
+            counter += 1;
+        }
+        lastnum = num;
+    }
+
     return counter;
 }
 
--- tests/aoc01_test.rs
+++ tests/aoc01_test.rs
@@ -15,4 +15,16 @@ mod tests {
         let num = sonar(String::from("./tests/challenge.input"));
         println!("{}", num)
     }
+
+    #[test]
+    fn sonar_window_test() {
+        let num = sonar_window(String::from("./tests/test.input"));
+        println!("{}", num)
+    }
+
+    #[test]
+    fn sonar_window_challenge() {
+        let num = sonar_window(String::from("./tests/challenge.input"));
+        println!("sonar_window_challenge {}", num)
+    }
 }
